16775eb6ae6dba69fccd27495c1d659ee8d6ecbc
Gary Taylor <gary.taylor@hismessages.com>
2017-05-12 07:30:09 +0100

    Initial template from bundler

---
+++ lib/shift/api/core.rb
@@ -0,0 +1,9 @@
+require "shift/api/core/version"
+
+module Shift
+  module Api
+    module Core
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/shift/api/core/version.rb
@@ -0,0 +1,7 @@
+module Shift
+  module Api
+    module Core
+      VERSION = "0.1.0"
+    end
+  end
+end
